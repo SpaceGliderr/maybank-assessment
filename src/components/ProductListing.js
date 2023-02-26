1a36499abd09198d4ff64e9aa9ebafe19bdca5cb
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-02-26 14:17:55 +0800

    Changed the ProductListing items from Box to Card

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,7 +1,11 @@
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Card, CardContent, Typography } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { getProducts } from "../api";
 
-const ProductListing = () => {
+const ProductListing = (props) => {
+  const { isDashboard } = props;
+
   const products = getProducts();
 
   const renderProducts = () => {
@@ -9,12 +13,41 @@ const ProductListing = () => {
       <>
         {Object.values(products).map((product) => {
           return (
-            <Box key={product.productSKU}>
-              <Typography variant="h6">{product.productName}</Typography>
-              <Typography variant="h6">
-                Quantity: {product.productQuantity}
-              </Typography>
-            </Box>
+            <Card
+              key={product.productSKU}
+              variant="outlined"
+              sx={{
+                margin: "0.5em 0 0.5em 0",
+                "& .MuiCardContent-root:last-child": {
+                  paddingBottom: "16px",
+                },
+              }}
+            >
+              <CardContent sx={{ display: "flex", alignItems: "center" }}>
+                <Box component="div" sx={{ flexGrow: "1" }}>
+                  <Typography variant="h6">{product.productName}</Typography>
+                  <Typography variant="h6">
+                    Quantity: {product.productQuantity}
+                  </Typography>
+                </Box>
+                {isDashboard && (
+                  <Box
+                    component="div"
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <IconButton>
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton>
+                      <DeleteIcon />
+                    </IconButton>
+                  </Box>
+                )}
+              </CardContent>
+            </Card>
           );
         })}
       </>
