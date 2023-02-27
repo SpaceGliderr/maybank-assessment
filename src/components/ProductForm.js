e4b07d482eff46ee44b0c3f3da1f36d813ab3ab7
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-02-27 10:31:22 +0800

    Add styling to ProductForm and support for editing product

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -9,8 +9,12 @@ import {
 import { useState } from "react";
 import { upsertProduct } from "../api";
 
-const ProductForm = () => {
-  const [productDetails, setProductDetails] = useState(null);
+const ProductForm = (props) => {
+  const { editProductDetails } = props;
+
+  const [productDetails, setProductDetails] = useState(
+    editProductDetails || { productQuantity: 0 }
+  );
 
   const onSubmitForm = (event) => {
     upsertProduct(productDetails);
@@ -27,48 +31,56 @@ const ProductForm = () => {
 
   return (
     <Box component="div" sx={{ display: "block" }}>
-      <Typography variant="h4">Create Product</Typography>
-      <Divider />
-      <Box component="form" onSubmit={onSubmitForm} sx={{ display: "block" }}>
+      <Typography variant="h4" sx={{ mt: "20px" }}>
+        {editProductDetails ? "Edit" : "Create"} Product
+      </Typography>
+      <Divider sx={{ m: "10px 0 20px" }} />
+      <Box
+        component="form"
+        onSubmit={onSubmitForm}
+        sx={{ display: "flex", flexDirection: "column", gap: "1em" }}
+      >
+        <FormLabel sx={{ display: "block" }}>Product SKU</FormLabel>
+        <TextField
+          name="productSKU"
+          variant="outlined"
+          placeholder="Enter product SKU"
+          value={productDetails.productSKU || ""}
+          onChange={onInputChange}
+          sx={{ width: "80%" }}
+        />
+        <FormLabel sx={{ display: "block" }}>Product Name</FormLabel>
+        <TextField
+          name="productName"
+          variant="outlined"
+          placeholder="Enter product name"
+          value={productDetails.productName || ""}
+          onChange={onInputChange}
+          sx={{ width: "80%" }}
+        />
+        {/* TODO: Change Product Quantity input to have two more buttons on each side to increase / decrease the quantity */}
+        <FormLabel sx={{ display: "block" }}>Product Quantity</FormLabel>
+        <TextField
+          name="productQuantity"
+          variant="outlined"
+          inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+          value={productDetails.productQuantity}
+          onChange={onInputChange}
+          sx={{ width: "80%" }}
+        />
         <Box component="div" sx={{ display: "block" }}>
-          <FormLabel sx={{ display: "block" }}>Product SKU</FormLabel>
-          <TextField
-            name="productSKU"
-            variant="outlined"
-            placeholder="Enter product SKU"
-            onChange={onInputChange}
-          />
-        </Box>
-        <Box component="div" sx={{ display: "block" }}>
-          <FormLabel sx={{ display: "block" }}>Product Name</FormLabel>
-          <TextField
-            name="productName"
-            variant="outlined"
-            placeholder="Enter product name"
-            onChange={onInputChange}
-          />
-        </Box>
-        <Box component="div" sx={{ display: "block" }}>
-          <FormLabel sx={{ display: "block" }}>Product Quantity</FormLabel>
-          <TextField
-            name="productQuantity"
-            variant="outlined"
-            inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-            placeholder="Enter product quantity"
-            onChange={onInputChange}
-          />
-        </Box>
-        <Box component="div" sx={{ display: "block" }}>
-          <Button type="submit">Create</Button>
-          <Button>Cancel</Button>
-          {/* TODO: Remove temp button to clear local storage */}
-          <Button
-            onClick={() => {
-              localStorage.clear();
-            }}
-          >
-            Clear
+          <Button type="submit">
+            {editProductDetails ? "Save" : "Create"}
           </Button>
+          {!editProductDetails && (
+            <Button
+              onClick={() => {
+                setProductDetails({ productQuantity: 0 });
+              }}
+            >
+              Clear
+            </Button>
+          )}
         </Box>
       </Box>
     </Box>
